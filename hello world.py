0487196ea089ef871c9d462eac103ee8aec79889
rifkypolanen <rifkiirwanto57@gmail.com>
2021-03-13 14:42:48 +0700

    Pertemuan Pertama

---
+++ hello world.py
@@ -0,0 +1,32 @@
+# This is a comment 
+
+'''
+ejfpsjlsdkselnlse
+fsejfpsfeepsj
+sofjespjfpesjf
+sofjpejsfje
+'''
+
+# print('Hello World')
+# print('Hello World')
+# print('Hello World')
+# print('Hello World')
+# print('Hello World')
+
+print('----------------------')
+
+x = int(5/2)
+y = 'hello world'
+z = True 
+
+print(type(x))
+print(x)
+print(type(y))
+print(type(z))
+
+print('----------------------')
+
+num = 2
+float_num = float(num)
+print(float_num)
+print(type(float_num))
